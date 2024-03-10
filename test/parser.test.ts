1802a06276b5943de8b2f9a5056a409b52ba5d82
Luis Rodríguez <luis.rodriguez@leanmind.es>
2024-03-10 15:15:14 +0100

    fix test

--- test/parser.test.ts
+++ test/parser.test.ts
@@ -3,18 +3,22 @@ import { Parser } from '../src/parser'
 import { mock } from 'vitest-mock-extended';
 import { describe, beforeEach, it, expect } from "vitest";
 import { TorrentFile } from "../src/torrentFile";
+import { Configuration } from "../src/configuration";
 
 describe('parser', function(){
     let fileReader : FileReader;
     let parser : Parser;
+    let configuration: Configuration;
 
     beforeEach(function(){
+        configuration = mock<Configuration>();
         fileReader = mock<FileReader>();
     });
 
     it('returns an empty torrent, when file is null', function(){
+        configuration.getConfiguration.mockReturnValue("configuration")
         fileReader.read.mockReturnValue("");
-        parser = new Parser(fileReader);
+        parser = new Parser(configuration, fileReader);
 
         const torrentFile = parser.parse();
 
