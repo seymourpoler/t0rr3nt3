ea75082532d146b7f9b161b0fc12a9b9d82043ff
Luis Rodríguez <luis.rodriguez@leanmind.es>
2026-04-06 18:00:40 +0200

    refactor Parser class constructor for brevity

--- src/parser.ts
+++ src/parser.ts
@@ -2,14 +2,8 @@ import { FileReader} from './fileReader'
 import { TorrentFile } from './torrentFile';
 import { Configuration } from './configuration';
 
-export class Parser{
-    private readonly fileReader: FileReader;
-    private readonly configuration: Configuration;
-
-    constructor(configuration: Configuration, fileReader: FileReader){
-        this.fileReader = fileReader;
-        this.configuration = configuration;
-    }
+export class Parser {
+    constructor(private readonly configuration: Configuration, private readonly fileReader: FileReader){}
 
     public parse() : TorrentFile {
         //const file = this.fileReader.read(this.configuration.getConfiguration());
